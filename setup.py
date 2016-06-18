c4c65337df2e169ae5c178dc79d25e8e234debd5
Syrus Akbary <me@syrusakbary.com>
2016-06-18 12:44:01 -0700

    Updated version to 0.4.4

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class PyTest(TestCommand):
 
 setup(
     name='graphql-relay',
-    version='0.4.3',
+    version='0.4.4',
 
     description='Relay implementation for Python',
     long_description=open('README.rst').read(),
