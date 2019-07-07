38cdb5357e0bdd80bff8c1ba3dd4dd0c0acd8ef7
Christoph Zwerschke <cito@online.de>
2019-07-07 18:00:50 +0200

    Test dict access in same module via fixture

--- tests/node/test_global.py
+++ tests/node/test_global.py
@@ -1,4 +1,6 @@
-from typing import Any, NamedTuple, Union
+from typing import Any, Dict, NamedTuple, Union
+
+from pytest import fixture
 
 from graphql import graphql_sync as graphql
 from graphql.type import (
@@ -13,113 +15,131 @@ from graphql.type import (
 from graphql_relay import from_global_id, global_id_field, node_definitions
 
 
-class User(NamedTuple):
-    id: int
-    name: str
+@fixture(scope='module', params=['object_access', 'dict_access'])
+def schema(request):
+    """Run each test with object access and dict access."""
+    use_dicts = request.param == 'dict_access'
 
+    if use_dicts:
 
-class Photo(NamedTuple):
-    photo_id: int
-    width: int
+        class User(dict):
+            pass
 
+        class Photo(dict):
+            pass
 
-class Post(NamedTuple):
-    id: int
-    text: str
+        class Post(dict):
+            pass
 
+    else:
 
-user_data = {
-    '1': User(id=1, name='John Doe'),
-    '2': User(id=2, name='Jane Smith'),
-}
+        class User(NamedTuple):
+            id: int
+            name: str
 
-photo_data = {
-    '1': Photo(photo_id=1, width=300),
-    '2': Photo(photo_id=2, width=400),
-}
+        class Photo(NamedTuple):
+            photo_id: int
+            width: int
 
-post_data = {
-    '1': Post(id=1, text='lorem'),
-    '2': Post(id=2, text='ipsum'),
-}
+        class Post(NamedTuple):
+            id: int
+            text: str
 
-
-def get_node(global_id: str, info: GraphQLResolveInfo) -> Union[User, Photo, Post]:
-    assert info.schema is schema
-    type_, id_ = from_global_id(global_id)
-    if type_ == 'User':
-        return user_data[id_]
-    if type_ == 'Photo':
-        return photo_data[id_]
-    if type_ == 'Post':
-        return post_data[id_]
-
-
-def get_node_type(
-        obj: Union[User, Photo], info: GraphQLResolveInfo,
-        _type: Any) -> GraphQLObjectType:
-    assert info.schema is schema
-    if isinstance(obj, User):
-        return user_type
-    if isinstance(obj, Photo):
-        return photo_type
-    if isinstance(obj, Post):
-        return post_type
-
-
-node_interface, node_field = node_definitions(get_node, get_node_type)[:2]
-
-user_type = GraphQLObjectType(
-    'User',
-    fields=lambda: {
-        'id': global_id_field('User'),
-        'name': GraphQLField(GraphQLString),
-    },
-    interfaces=[node_interface]
-)
-
-photo_type = GraphQLObjectType(
-    'Photo',
-    fields=lambda: {
-        'id': global_id_field('Photo', lambda obj, _info: obj.photo_id),
-        'width': GraphQLField(GraphQLInt),
-    },
-    interfaces=[node_interface]
-)
-
-post_type = GraphQLObjectType(
-    'Post',
-    fields=lambda: {
-        'id': global_id_field(),
-        'text': GraphQLField(GraphQLString),
-    },
-    interfaces=[node_interface]
-)
-
-query_type = GraphQLObjectType(
-    'Query',
-    fields=lambda: {
-        'node': node_field,
-        'allObjects': GraphQLField(
-            GraphQLList(node_interface),
-            resolve=lambda _root, _info: [
-                user_data['1'], user_data['2'],
-                photo_data['1'], photo_data['2'],
-                post_data['1'], post_data['2']
-            ]
-        )
+    user_data = {
+        '1': User(id=1, name='John Doe'),
+        '2': User(id=2, name='Jane Smith'),
     }
-)
 
-schema = GraphQLSchema(
-    query=query_type,
-    types=[user_type, photo_type, post_type]
-)
+    photo_data = {
+        '1': Photo(photo_id=1, width=300),
+        '2': Photo(photo_id=2, width=400),
+    }
+
+    post_data = {
+        '1': Post(id=1, text='lorem'),
+        '2': Post(id=2, text='ipsum'),
+    }
+
+    def get_node(
+            global_id: str, info: GraphQLResolveInfo
+    ) -> Union[User, Photo, Post, Dict]:
+        assert info.schema is schema
+        type_, id_ = from_global_id(global_id)
+        if type_ == 'User':
+            return user_data[id_]
+        if type_ == 'Photo':
+            return photo_data[id_]
+        if type_ == 'Post':
+            return post_data[id_]
+
+    def get_node_type(
+            obj: Union[User, Photo], info: GraphQLResolveInfo,
+            _type: Any) -> GraphQLObjectType:
+        assert info.schema is schema
+        if 'name' in obj if use_dicts else isinstance(obj, User):
+            return user_type
+        if 'photo_id' in obj if use_dicts else isinstance(obj, Photo):
+            return photo_type
+        if 'text' in obj if use_dicts else isinstance(obj, Post):
+            return post_type
+
+    node_interface, node_field = node_definitions(get_node, get_node_type)[:2]
+
+    user_type = GraphQLObjectType(
+        'User',
+        fields=lambda: {
+            'id': global_id_field('User'),
+            'name': GraphQLField(GraphQLString),
+        },
+        interfaces=[node_interface]
+    )
+
+    photo_type = GraphQLObjectType(
+        'Photo',
+        fields=lambda: {
+            'id': global_id_field(
+                'Photo',
+                lambda obj, _info: obj['photo_id'] if use_dicts else obj.photo_id),
+            'width': GraphQLField(GraphQLInt),
+        },
+        interfaces=[node_interface]
+    )
+
+    post_type = GraphQLObjectType(
+        'Post',
+        fields=lambda: {
+            'id': global_id_field(),
+            'text': GraphQLField(GraphQLString),
+        },
+        interfaces=[node_interface]
+    )
+
+    query_type = GraphQLObjectType(
+        'Query',
+        fields=lambda: {
+            'node': node_field,
+            'allObjects': GraphQLField(
+                GraphQLList(node_interface),
+                resolve=lambda _root, _info: [
+                    user_data['1'], user_data['2'],
+                    photo_data['1'], photo_data['2'],
+                    post_data['1'], post_data['2']
+                ]
+            )
+        }
+    )
+
+    schema = GraphQLSchema(
+        query=query_type,
+        types=[user_type, photo_type, post_type]
+    )
+
+    yield schema
 
 
 def describe_global_id_fields():
 
-    def gives_different_ids():
+    def gives_different_ids(schema):
         query = '''
         {
           allObjects {
@@ -153,7 +173,7 @@ def describe_global_id_fields():
             None
         )
 
-    def refetches_the_ids():
+    def refetches_the_ids(schema):
         query = '''
         {
           user: node(id: "VXNlcjox") {
