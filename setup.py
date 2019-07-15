ac277af2fe2b54cf64296628b50241c5e0b857d7
Mel van Londen <melvanlonden@gmail.com>
2019-07-15 13:08:48 -0700

    increment version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class PyTest(TestCommand):
 
 setup(
     name='graphql-relay',
-    version='0.4.5',
+    version='0.4.6',
 
     description='Relay implementation for Python',
     long_description=open('README.rst').read(),
